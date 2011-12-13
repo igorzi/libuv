6d5ac9f2fc2cecd6a044d57ac2884ff67165f734
Igor Zinkovsky <igorzi@microsoft.com>
2011-12-13 01:29:27 -0800

    platform api

--- src/unix/darwin.c
+++ src/unix/darwin.c
@@ -25,6 +25,9 @@
 #include <stdint.h>
 #include <errno.h>
 
+#include <ifaddrs.h>
+#include <net/if.h>
+
 #include <CoreServices/CoreServices.h>
 #include <mach/mach.h>
 #include <mach/mach_time.h>
@@ -32,6 +35,9 @@
 #include <sys/resource.h>
 #include <sys/sysctl.h>
 #include <unistd.h>  /* sysconf */
+
+
+static char *process_title;
 
 
 uint64_t uv_hrtime() {
@@ -71,6 +77,7 @@ int uv_exepath(char* buffer, size_t* size) {
   return 0;
 }
 
+
 uint64_t uv_get_free_memory(void) {
   vm_statistics_data_t info;
   mach_msg_type_number_t count = sizeof(info) / sizeof(integer_t);
@@ -82,6 +89,7 @@ uint64_t uv_get_free_memory(void) {
 
   return (uint64_t) info.free_count * sysconf(_SC_PAGESIZE);
 }
+
 
 uint64_t uv_get_total_memory(void) {
   uint64_t info;
@@ -95,6 +103,7 @@ uint64_t uv_get_total_memory(void) {
   return (uint64_t) info;
 }
 
+
 void uv_loadavg(double avg[3]) {
   struct loadavg info;
   size_t size = sizeof(info);
@@ -105,4 +114,309 @@ void uv_loadavg(double avg[3]) {
   avg[0] = (double) info.ldavg[0] / info.fscale;
   avg[1] = (double) info.ldavg[1] / info.fscale;
   avg[2] = (double) info.ldavg[2] / info.fscale;
+}
+
+
+char** uv_setup_args(int argc, char** argv) {
+  process_title = argc ? strdup(argv[0]) : NULL;
+  return argv;
+}
+
+
+uv_err_t uv_set_process_title(const char* title) {
+  /* 
+   * Warning: here be dragons! This is SPI reverse-engineered from WebKit's
+   * plugin host, and could break at any time (although realistically it's only
+   * likely to break in a new major release).
+   * When 10.7 is available, check that this still works, and update this
+   * comment for 10.8.
+   */
+
+  /* Private CFType used in these LaunchServices calls. */
+  typedef CFTypeRef PrivateLSASN;
+  typedef PrivateLSASN (*LSGetCurrentApplicationASNType)();
+  typedef OSStatus (*LSSetApplicationInformationItemType)(int, PrivateLSASN,
+                                                          CFStringRef,
+                                                          CFStringRef,
+                                                          CFDictionaryRef*);
+
+  static LSGetCurrentApplicationASNType ls_get_current_application_asn_func =
+      NULL;
+  static LSSetApplicationInformationItemType
+      ls_set_application_information_item_func = NULL;
+  static CFStringRef ls_display_name_key = NULL;
+
+  static int symbol_lookup_status = 0; /* 1=ok, 2=unavailable */
+
+  /* Constant used by WebKit; what exactly it means is unknown. */
+  const int magic_session_constant = -2;
+
+  CFBundleRef launch_services_bundle;
+  CFStringRef* key_pointer;
+  PrivateLSASN asn;
+  CFStringRef process_name;
+  OSErr err;
+
+  if (symbol_lookup_status == 2) {
+    /* feature is unavailable */
+    return uv__new_artificial_error(UV_EINVAL);
+  }
+
+  if (process_title) free(process_title);
+  process_title = strdup(title);
+
+  if (!symbol_lookup_status) {
+    launch_services_bundle =
+        CFBundleGetBundleWithIdentifier(CFSTR("com.apple.LaunchServices"));
+    if (!launch_services_bundle) {
+      symbol_lookup_status = 2;
+      return uv__new_artificial_error(UV_EINVAL);
+    }
+
+    ls_get_current_application_asn_func =
+        (LSGetCurrentApplicationASNType)(
+            CFBundleGetFunctionPointerForName(
+                launch_services_bundle, CFSTR("_LSGetCurrentApplicationASN")));
+    if (!ls_get_current_application_asn_func) {
+      symbol_lookup_status = 2;
+      return uv__new_artificial_error(UV_EINVAL);
+    }
+
+    ls_set_application_information_item_func =
+        (LSSetApplicationInformationItemType*)(
+            CFBundleGetFunctionPointerForName(
+                launch_services_bundle,
+                CFSTR("_LSSetApplicationInformationItem")));
+    if (!ls_set_application_information_item_func) {
+      symbol_lookup_status = 2;
+      return uv__new_artificial_error(UV_EINVAL);
+    }
+
+    key_pointer = (const CFStringRef*)(
+        CFBundleGetDataPointerForName(launch_services_bundle,
+                                      CFSTR("_kLSDisplayNameKey")));
+    ls_display_name_key = key_pointer ? *key_pointer : NULL;
+    if (!ls_display_name_key) {
+      symbol_lookup_status = 2;
+      return uv__new_artificial_error(UV_EINVAL);
+    }
+
+    /*
+     * Internally, this call relies on the Mach ports that are started up by the
+     * Carbon Process Manager.  In debug builds this usually happens due to how
+     * the logging layers are started up; but in release, it isn't started in as
+     * much of a defined order.  So if the symbols had to be loaded, go ahead
+     * and force a call to make sure the manager has been initialized and hence
+     * the ports are opened.
+     */
+    ProcessSerialNumber psn;
+    GetCurrentProcess(&psn);
+    symbol_lookup_status = 1; // 1=ok
+  }
+
+  asn = ls_get_current_application_asn_func();
+  process_name =
+    CFStringCreateWithCString(NULL, title, kCFStringEncodingUTF8);
+  err =
+    ls_set_application_information_item_func(magic_session_constant, asn,
+                                             ls_display_name_key,
+                                             process_name,
+                                             NULL /* optional out param */);
+
+  if (err) {
+    return uv__new_sys_error(errno);
+  }
+
+  return uv_ok_;
+}
+
+
+uv_err_t uv_get_process_title(char* buffer, size_t size) {
+  if (process_title) {
+    strncpy(buffer, process_title, size);
+  } else {
+    if (size > 0) {
+      buffer[0] = '\0';
+    }
+  }
+
+  return uv_ok_;
+}
+
+
+uv_err_t uv_resident_set_memory(size_t* rss) {
+  struct task_basic_info t_info;
+  mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+  int r = task_info(mach_task_self(),
+                    TASK_BASIC_INFO,
+                    (task_info_t)&t_info,
+                    &t_info_count);
+
+  if (r != KERN_SUCCESS) {
+    return uv__new_sys_error(errno);
+  }
+
+  *rss = t_info.resident_size;
+
+  return uv_ok_;
+}
+
+
+uv_err_t uv_uptime(double* uptime) {
+  time_t now;
+  struct timeval info;
+  size_t size = sizeof(info);
+  static int which[] = {CTL_KERN, KERN_BOOTTIME};
+
+  if (sysctl(which, 2, &info, &size, NULL, 0) < 0) {
+    return uv__new_sys_error(errno);
+  }
+  now = time(NULL);
+
+  *uptime = (double)(now - info.tv_sec);
+
+  return uv_ok_;
+}
+
+uv_err_t uv_cpu_info(uv_cpu_info_t** cpu_infos, int* count) {
+  unsigned int ticks = (unsigned int)sysconf(_SC_CLK_TCK),
+               multiplier = ((uint64_t)1000L / ticks);
+  char model[512];
+  uint64_t cpuspeed;
+  size_t size;
+  unsigned int i;
+  natural_t numcpus;
+  mach_msg_type_number_t count;
+  processor_cpu_load_info_data_t *info;
+  uv_cpu_info_t* cpu_info;
+
+  size = sizeof(model);
+  if (sysctlbyname("hw.model", &model, &size, NULL, 0) < 0) {
+    return uv__new_sys_error(errno);
+  }
+  size = sizeof(cpuspeed);
+  if (sysctlbyname("hw.cpufrequency", &cpuspeed, &size, NULL, 0) < 0) {
+    return uv__new_sys_error(errno);
+  }
+
+  if (host_processor_info(mach_host_self(), PROCESSOR_CPU_LOAD_INFO, &numcpus,
+                          (processor_info_array_t*)&info,
+                          &count) != KERN_SUCCESS) {
+    return uv__new_sys_error(errno);
+  }
+
+  *cpu_infos = (uv_cpu_info_t*)malloc(numcpus * sizeof(uv_cpu_info_t));
+  if (!(*cpu_infos)) {
+    return uv__new_artificial_error(UV_ENOMEM);
+  }
+
+  *count = numcpus;
+
+  for (i = 0; i < numcpus; i++) {
+    cpu_info = &(*cpu_infos)[i];
+
+    cpu_info->cpu_times.user = (uint64_t)(info[i].cpu_ticks[0]) * multiplier);
+    cpu_info->cpu_times.nice = (uint64_t)(info[i].cpu_ticks[3]) * multiplier);
+    cpu_info->cpu_times.sys = (uint64_t)(info[i].cpu_ticks[1]) * multiplier);
+    cpu_info->cpu_times.idle = (uint64_t)(info[i].cpu_ticks[2]) * multiplier);
+    cpu_info->cpu_times.irq = 0;
+
+    cpu_info->model = strdup(model);
+    cpu_info->speed = cpuspeed/1000000;
+  }
+  vm_deallocate(mach_task_self(), (vm_address_t)info, count);
+
+  return 0;
+}
+
+
+void uv_free_cpu_info(uv_cpu_info_t* cpu_infos, int count) {
+  int i;
+
+  for (i = 0; i < count; i++) {
+    free(cpu_infos[i].model);
+  }
+
+  free(cpu_infos);
+}
+
+
+uv_err_t uv_interface_addresses(uv_interface_address_t** addresses,
+  int* count) {
+  struct ifaddrs *addrs, *ent;
+  char ip[INET6_ADDRSTRLEN];
+  uv_interface_address_t* address;
+
+  if (getifaddrs(&addrs) != 0) {
+    return uv__new_sys_error(errno);
+  }
+
+  *count = 0;
+
+  /* Count the number of interfaces */
+  for (ent = addrs; ent != NULL; ent = ent->ifa_next) {
+    if (!(ent->ifa_flags & IFF_UP && ent->ifa_flags & IFF_RUNNING) ||
+        (ent->ifa_addr == NULL) ||
+        (ent->ifa_addr->sa_family == AF_LINK)) {
+      continue;
+    }
+
+    (*count)++;
+  }
+
+  *addresses = (uv_interface_address_t*)
+    malloc(*count * sizeof(uv_interface_address_t));
+  if (!(*addresses)) {
+    return uv__new_artificial_error(UV_ENOMEM);
+  }
+
+  address = *addresses;
+
+  for (ent = addrs; ent != NULL; ent = ent->ifa_next) {
+    bzero(&ip, sizeof (ip));
+    if (!(ent->ifa_flags & IFF_UP && ent->ifa_flags & IFF_RUNNING)) {
+      continue;
+    }
+
+    if (ent->ifa_addr == NULL) {
+      continue;
+    }
+
+    /*
+     * On Mac OS X getifaddrs returns information related to Mac Addresses for
+     * various devices, such as firewire, etc. These are not relevant here.
+     */
+    if (ent->ifa_addr->sa_family == AF_LINK) {
+      continue;
+    }
+
+    address->name = strdup(ent->ifa_name);
+
+    if (ent->ifa_addr->sa_family == AF_INET6) {
+      address->address.address6 = *((struct sockaddr_in6 *)ent->ifa_addr);
+    } else {
+      address->address.address4 = *((struct sockaddr_in *)ent->ifa_addr);
+    }
+
+    address->is_internal = ent->ifa_flags & IFF_LOOPBACK ? 1 : 0;
+
+    address++;
+  }
+
+  freeifaddrs(addrs);
+
+  return uv_ok_;
+}
+
+
+void uv_free_interface_addresses(uv_interface_address_t* addresses,
+  int count) {
+  int i;
+
+  for (i = 0; i < count; i++) {
+    free(addresses[i].name);
+  }
+
+  free(addresses);
 }
